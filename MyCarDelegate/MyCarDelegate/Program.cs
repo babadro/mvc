506338f4ca7699b70c4cb999b1189c3e4c66198b
babadro <baba_dro@mail.ru>
2015-01-20 12:08:25 +0300

    Начальное состояние

---
+++ MyCarDelegate/MyCarDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Delegates as event enablers *****\n");
+
+            Car c1 = new Car("SlugBug", 100, 10);
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+        }
+
+        #region Delegate tatgets
+        public static void OnCarEngineEvent( string msg )
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***************************************\n");
+        }
+
+        public static void onCarEngineEvent2( string msg )
+        {
+            Console.WriteLine("=> {0}", msg.ToUpper());
+        }
+        #endregion
+    }
+}
---
+++ Parser2/Parser2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+
+namespace Parser2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            string k = "<html><head><title>Пример 1</title></head> <body> <div class=\"bla1\">Это простейший пример HTML-документа.</div></body> </html>";
+            doc.LoadHtml(k);
+            HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//div[@class='bla1']");
+            Console.WriteLine(bodyNode.InnerText);
+            Console.ReadLine();
+        }
+    }
+}
