8b0fef828044e4f228b0f32c337525bc074d0938
andrey2 <baba_dro@mail.ru>
2015-01-22 16:20:33 +0300

    101

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -95,6 +96,85 @@ namespace LanguageFeatures.Controllers
 
             return View("Result",
                 (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart {
+                Products = new List<Product> {
+                new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+                new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+                new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer" || prod.Price > 20 )) {
+                total += prod.Price; }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
+
+        public ViewResult CreateAnonArray()
+        {
+            var oddsAndEnds = new[] {
+                new { Name = "MVC", Category = "Pattern" },
+                new { Name = "Hat", Category = "Clothing" },
+                new { Name = "Apple", Category = "Fruit" }
+            };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var item in oddsAndEnds) {
+                result.Append(item.Name).Append(" ");
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult FindProducts()
+        {
+            Product[] products = {
+                                    new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                                    new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                                    new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                                    new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+                                 };
+
+            var foundProducts = products.OrderByDescending(e => e.Price)
+                .Take(3)
+                .Select(e => new
+                {
+                    e.Name,
+                    e.Price
+                });
+
+            products[2] = new Product { Name = "Stadium", Price = 79600M };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ViewResult SumProducts()
+        {
+            Product[] products = {
+                                    new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                                    new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                                    new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                                    new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+                                 };
+
+            var results = products.Sum(e => e.Price);
+
+            products[2] = new Product { Name = "Stadium", Price = 79500M };
+
+            return View("Result",
+                (object)String.Format("Sum: {0:c}", results));
         }
     }
 }
