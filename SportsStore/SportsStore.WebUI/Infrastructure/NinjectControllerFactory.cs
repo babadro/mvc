30e780f2b27e2b3e5b831a00802418cd35e7b2f1
andrey2 <baba_dro@mail.ru>
2015-02-03 15:17:08 +0300

    165

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory
+    {
+    }
+}
