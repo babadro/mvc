d001100e1c983c5f20cec1628542de3a12f370eb
babadro <baba_dro@mail.ru>
2015-02-03 13:03:21 +0300

    152

--- EssentialTools/EssentialTools.Tests/UnitTest2.cs
+++ EssentialTools/EssentialTools.Tests/UnitTest2.cs
@@ -31,5 +31,38 @@ namespace EssentialTools.Tests
             // assert
             Assert.AreEqual(products.Sum(e => e.Price), result);
         }
+
+        private Product[] createProduct(decimal value)
+        {
+            return new[] {
+             new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            // arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0)))
+                .Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100)))
+                .Returns<decimal>(total => (total * 0.9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100,
+                Range.Inclusive))).Returns<decimal>(total => total - 5);
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            decimal FiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(createProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(createProduct(100));
+            decimal FiveHundredDOllarDiscount = target.ValueProducts(createProduct(500));
+
+            // assert
+            Assert.AreEqual()
+        }
     }
 }
