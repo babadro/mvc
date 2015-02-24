dfe414a70e1a52df05bd830c0244b24291aa693c
andrey2 <baba_dro@mail.ru>
2015-02-24 11:15:59 +0300

    stage 2 create (DI)

---
+++ myEssentTools - stage2/myEssentTools/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿
+namespace myEssentTools.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int ProductID { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+
+    }
+}
