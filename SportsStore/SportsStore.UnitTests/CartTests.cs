d3001d1e98f0809b954e75321728267a61ba5778
andrey2 <baba_dro@mail.ru>
2015-06-25 11:58:46 +0300

    page 212

---
+++ SportsStore/SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
